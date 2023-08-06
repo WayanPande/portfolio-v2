a7749798db322d51fe342db5cea049cef51bc686
WayanPande <yande554@gmail.com>
2023-08-06 10:37:14 +0700

    add base path

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = "";
+let basePath = "";
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
   assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
 const nextConfig = {
@@ -17,6 +19,7 @@ const nextConfig = {
     path: "",
   },
   assetPrefix: assetPrefix,
+  basePath: basePath,
   output: "export",
 };
 
