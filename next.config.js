e6109e3ef887eabf3d7de250f740de3f0b7638e0
WayanPande <yande554@gmail.com>
2023-08-06 10:10:21 +0700

    add github pages workflows

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  // assetPrefix: "./",
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
