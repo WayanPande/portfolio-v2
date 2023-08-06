280b943d72ac4dfc10cbd198a2e757e76e2a72b7
WayanPande <yande554@gmail.com>
2023-08-06 10:33:49 +0700

    try fix next config for deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+}
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "akamai",
     path: "",
   },
-  assetPrefix: "./",
+  assetPrefix: assetPrefix,
   output: "export",
 };
 
