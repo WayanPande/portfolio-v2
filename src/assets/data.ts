1060d9c7110bddb8be7e0c5176919e4c4115ed9c
WayanPande <yande554@gmail.com>
2023-08-06 09:50:08 +0700

    add hero section

---
+++ src/assets/data.ts
@@ -0,0 +1,20 @@
+export const DATA = [
+    {
+        id: 1,
+        name: 'Antrie',
+        date: '2021',
+        description: "Antrie is a responsive web-based queueing system that will help public facilities like public health centers, vaccine places, and any other places that allows each user to queue and make a queue easily.",
+        roleDesc: "My role in this project was to mainly to build the web mockup using Figma and convert it into a working front-end mainly using Html, CSS, and Bootstrap 5.",
+        techStack: ["html", "javascript", "css", "laravel 8", "Jquery", "MySQL"],
+        img: 'antrie.JPG'
+    },
+    {
+        id: 2,
+        name: 'BBH',
+        date: '2021',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tristique, metus eget varius congue, eros neque hendrerit ex, eu tempus lorem tellus sit amet tortor. Phasellus quis luctus mauris. Integer blandit augue odio, ut finibus nisi maximus in. Cras eget vestibulum lacus. Fusce nec metus ac orci commodo rhoncus. Sed neque leo, consequat quis orci sit amet, accumsan molestie erat. Nam vulputate nisl vitae nisi pretium, id porttitor ante bibendum. Suspendisse feugiat consequat consequat. Integer ut pharetra massa.",
+        roleDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tristique, metus eget varius congue, eros neque hendrerit ex, eu tempus lorem tellus sit amet tortor. Phasellus quis luctus mauris. Integer blandit augue odio, ut finibus nisi maximus in.",
+        techStack: ["ReactJS", "NodeJS", "NextJS", "MongoDB"],
+        img: 'bbh.JPG'
+    },
+]
